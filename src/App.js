458d40b59669e2b1324a67565f854085efa636ea
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-24 22:16:23 -0300

    tela de reserva
    Criação da tela de reserva do restaurante

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Login from './components/Login';
+//import Login from './components/Login';
+import Reserva from './components/Reserva'
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <body>
+      <Reserva/>
+    </body>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,20 +4,22 @@ import imgFundo from './img/imagelogin.png'
 
 function Login(){
     return (
-    <div className={styles.container}>
-            <img className={styles.imgFundo} src={imgFundo} alt="fundo"/>
-            <div className={styles.containerLogin}>
-            <div className={styles.containerBox}>
-                <img className={styles.Logo} src={Logo} alt = "logo"/>
-                    <form action="">
-                        <input className={styles.input} type="text" placeholder="Login"/> <br/>
-                        <input className={styles.input} type="text" placeholder="Senha"/> <br/>
-                        <button className={styles.buttonLogin}>Entrar</button>
-                        <button className={styles.buttonCadastro}>Cadastrar</button>
-                    </form>
-            </div>
-            </div>
-    </div>
+    <body>
+        <div className={styles.container}>
+                <img className={styles.imgFundo} src={imgFundo} alt="fundo"/>
+                <div className={styles.containerLogin}>
+                <div className={styles.containerBox}>
+                    <img className={styles.Logo} src={Logo} alt = "logo"/>
+                        <form action="">
+                            <input className={styles.input} type="text" placeholder="Login"/> <br/>
+                            <input className={styles.input} type="text" placeholder="Senha"/> <br/>
+                            <button className={styles.buttonLogin}>Entrar</button>
+                            <button className={styles.buttonCadastro}>Cadastrar</button>
+                        </form>
+                </div>
+                </div>
+        </div>
+    </body>
     )
 }
 
