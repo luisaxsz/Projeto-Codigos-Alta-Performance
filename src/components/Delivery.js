5afcfece6479f14ff9fcbf788016dcf813257ec4
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-29 11:38:56 -0300

    Atualização em estilo
    Alteração no estilo da página. Criação de buttons para toggle list e alteração nas divs centrais de lista de endereço, formulario de endereço e linha central.

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,4 +1,4 @@
-import "./delivery.css";
+import  "./delivery.css";
 import Logo002 from "./imgs/logo_mauricio.png";
 import fundo from "./imgs/japanese-food-4984960.jpg";
 
@@ -41,36 +41,41 @@ function Delivery() {
             <label for="opcao3"></label>
           </div>
         </div>
-        <div id="endereco">
-            <h2>ENDEREÇO</h2>
-            <div>
-                <p>Endereço 1</p>
-                <label>Rua</label>
-                <input type="text" placeholder="tal de tal de tal"/>
-                <label>Num. da Casa</label>
-                <input type="number" placeholder="tal de tal de tal"/>
-                <label>Complemento</label>
-                <input type="text" placeholder="tal de tal de tal"/>
-                <button>Adicionar Endereço</button>
-                <p>Endereço 2 <span>+</span></p>
-                <p>Endereço 3 <span>+</span></p>
-                <p>Endereço 4 <span>+</span></p>
-            </div>
-        </div>
-        <div id="linha"></div>
-        <div id="carrinho">
-            <h2>CARRINHO</h2>
-            <div id="Items">
-                <p>Itens Adicionados</p>
-                <div></div> 
+        <div id="boxContainer">
+          <div id="endereco">
+              <h2>ENDEREÇO</h2>
+              <div>
+                  <p>Endereço 1</p>
+                  <label>Rua</label>
+                  <input type="text" placeholder="tal de tal de tal"/>
+                  <label>Num. da Casa</label>
+                  <input type="number" placeholder="tal de tal de tal"/>
+                  <label>Complemento</label>
+                  <input type="text" placeholder="tal de tal de tal"/>
+                  <button>Adicionar Endereço</button>
+                  <p>Endereço 2 <span>+</span></p>
+                  <p>Endereço 3 <span>+</span></p>
+                  <p>Endereço 4 <span>+</span></p>
+              </div>
+              <button id="addEnd">Novo endereco</button>
+          </div>
+          <div id="linha"></div>
+          <div id="carrinhoContainer">
+            <div id="carrinho">
+                <h2>CARRINHO</h2>
+                <div id="Items">
+                    <p>Itens Adicionados</p>
+                    <div></div>
+                </div>
             </div>
             <div id="locations">
-                <h3>Endereço Utilizado</h3>
-                <p id="check">Endereço 1 <span>v</span></p>
-                <p>Endereço 2 <span>+</span></p>
-                <p>Endereço 3 <span>+</span></p>
-                <p>Endereço 4 <span>+</span></p>
+              <h3>Endereço Utilizado</h3>
+              <p id="check">Endereço 1 <span>v</span></p>
+              <p>Endereço 2 <span>+</span></p>
+              <p>Endereço 3 <span>+</span></p>
+              <p>Endereço 4 <span>+</span></p>
             </div>
+          </div>
         </div>
       </main>
     </div>
