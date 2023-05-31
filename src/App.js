e9c38264827525114d37a87ac2f9ea243ab9603b
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-31 19:29:32 -0300

    atualização na pag reserva

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Delivery from './components/Delivery/Delivery';
-
+import Reserva from './components/Reserva/Reserva'
 function App() {
   return (
     <body>
-      <Delivery/>
+      <Reserva/>
     </body>
   );
 }
